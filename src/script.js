6f2e0d8da5c02b6925749a4cb457e2163b062c2a
Mangwa Boris <borisweteneyi1@gmail.com>
2026-07-15 14:19:50 +0100

    Add form

---
+++ src/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", () => {
+  const form = document.querySelector("form");
+  const email_error = document.getElementById("email_error");
+  const name_error = document.getElementById("name_error");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email");
+    const name = document.getElementById("name");
+
+    if (name.value.trim() === "" || parseInt(name.value)) {
+      name.classList.add("border-red-500");
+      name_error.classList.replace("hidden", "flex");
+    } else {
+      name.classList.remove("border-red-500");
+      name_error.classList.replace("flex", "hidden");
+    }
+    if (email.value.trim() === "") {
+      email.classList.add("border-red-500");
+      email_error.classList.replace("hidden", "flex");
+    } else {
+      email.classList.remove("border-red-500");
+      email_error.classList.replace("flex", "hidden");
+    }
+  });
+});
